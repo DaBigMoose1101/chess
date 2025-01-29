7070eaba38b70882764288bde3f800c10721c846
DaBigMoose1101 <porter.t.nichols@gmail.com>
2025-01-29 10:46:31 -0700

    Began implementing ChessGame.java by setting up class variables and constructor. I implemented some getter functions.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -9,16 +9,19 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessGame {
-
+    TeamColor turn;
+    ChessBoard board;
     public ChessGame() {
-
+        this.board = new ChessBoard();
+        this.turn = TeamColor.WHITE;
+        board.resetBoard();
     }
 
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return turn;
     }
 
     /**
@@ -27,7 +30,7 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        this.turn = team;
     }
 
     /**
