8fbeaecdf7559302e7d4eb254f61910437a7cf65
DaBigMoose1101 <porter.t.nichols@gmail.com>
2025-03-03 12:41:43 -0700

    Edited GameDAO.java setting up different methods.

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,16 +1,25 @@
 package dataaccess;
+import java.util.ArrayList;
+import chess.ChessGame;
 
 public class GameDAO {
     public void createGame(){
 
     }
-    public void readGame(){
+    public void getGame(){
 
+    }
+    public ArrayList<String> listGames(){
+
+        return null;
     }
     public void updateGame(){
 
     }
     public void deleteGame(){
+
+    }
+    public void joinGame(ChessGame game){
 
     }
 }
