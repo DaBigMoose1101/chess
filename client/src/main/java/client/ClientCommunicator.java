e5cf56d3e89c702000c18b70d9a531b2f1778cb6
DaBigMoose1101 <porter.t.nichols@gmail.com>
2025-03-21 20:13:26 -0600

    Set up ServerFacade.java and ClientCommunicator.java

--- client/src/main/java/client/ClientCommunicator.java
+++ client/src/main/java/client/ClientCommunicator.java
@@ -1,4 +1,32 @@
 package client;
 
+import java.net.*;
+
 public class ClientCommunicator {
+
+    private void setRequestBody(){
+
+    }
+
+    private void setRequestHeader(){
+
+    }
+
+    public ClientCommunicator(){
+
+    }
+
+    public void getRequest(){
+
+    }
+
+    public void postRequest(){
+
+    }
+
+    public void putRequest(){
+
+    }
+
+
 }
--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,4 +1,20 @@
 package client;
 
+import client.ClientCommunicator;
+import records.RegisterRequest;
+
 public class ServerFacade {
+    ClientCommunicator communicator;
+    String url;
+    public ServerFacade(){
+        this.url = "example url";
+        communicator = new ClientCommunicator();
+    }
+
+    public Object register(String username, String password, String email){
+        RegisterRequest req = new RegisterRequest(username, password, email);
+
+
+        return null;
+    }
 }
