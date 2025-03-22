8f4f685dd713571d626b827f43ed529be403443a
DaBigMoose1101 <porter.t.nichols@gmail.com>
2025-03-21 20:43:23 -0600

    Made changes to ServerFacade.java

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -5,12 +5,12 @@ import client.ClientCommunicator;
 import com.google.gson.Gson;
 import records.*;
 
+
+
 public class ServerFacade {
-    ClientCommunicator communicator;
-    String url;
+    String serverURL;
     public ServerFacade(){
-        this.url = "example url";
-        communicator = new ClientCommunicator();
+        this.serverURL = "example url";
     }
 
     public Object register(String username, String password, String email){
@@ -18,10 +18,17 @@ public class ServerFacade {
         Gson serializer = new Gson();
         RegisterRequest req = new RegisterRequest(username, password, email);
         String body = serializer.toJson(req);
+        ClientCommunicator comm = new ClientCommunicator();
+        try{
+            comm.postRequest(serverURL + path, null, body);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
 
         return null;
     }
+
     public Object login(String username, String password){
         String path = "/user";
         Gson serializer = new Gson();
@@ -30,6 +37,7 @@ public class ServerFacade {
 
         return null;
     }
+
     public Object logout(String authToken){
         String path = "/user";
         Gson serializer = new Gson();
@@ -38,14 +46,22 @@ public class ServerFacade {
 
         return null;
     }
+
     public Object createGame(String authToken, String gameName){
         String path = "/game";
         Gson serializer = new Gson();
         CreateGameRequest req = new CreateGameRequest(gameName);
         String body = serializer.toJson(req);
+        ClientCommunicator comm = new ClientCommunicator();
+        try{
+            comm.postRequest(serverURL + path, authToken, body);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         return null;
     }
+
     public Object getGameList(String authToken){
         String path = "/game";
         Gson serializer = new Gson();
@@ -54,6 +70,7 @@ public class ServerFacade {
 
         return null;
     }
+
     public Object joinGame(String authToken, ChessGame.TeamColor color, int gameId){
         String path = "/game";
         Gson serializer = new Gson();
