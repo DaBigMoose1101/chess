6b1b3b47349abe92e8105673cdd10fa595f1c5f0
DaBigMoose1101 <porter.t.nichols@gmail.com>
2025-04-03 12:14:05 -0600

    Added ErrorMessage.java and MakeMoveCommand.java outlined handler switch cases and WebSocketService methods

--- server/src/main/java/handler/WebSocketHandler.java
+++ server/src/main/java/handler/WebSocketHandler.java
@@ -6,26 +6,57 @@ import dataaccess.GameDAO;
 import org.eclipse.jetty.websocket.api.*;
 import org.eclipse.jetty.websocket.api.annotations.*;
 import service.WebSocketService;
+import websocket.commands.MakeMoveCommand;
 import websocket.commands.UserGameCommand;
+import websocket.messages.ErrorMessage;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
 
 public class WebSocketHandler {
     static AuthDAO authDataAccess;
     static GameDAO gameDataAccess;
     public final WebSocketService service = new WebSocketService(authDataAccess, gameDataAccess);
+    Gson serializer = new Gson();
 
     static void initialize(AuthDAO auth, GameDAO game){
         authDataAccess = auth;
         gameDataAccess = game;
+
     }
 
     @OnWebSocketMessage
     public void onMessage(Session session, String message){
-
-        Gson serializer = new Gson();
         UserGameCommand com = serializer.fromJson(message, UserGameCommand.class);
-        switch(com.getCommandType()){
-            case CONNECT -> service.connectPlayer(session, com);
+        ServerMessage serverMessage;
+        try {
+            switch (com.getCommandType()) {
+                case CONNECT:
+                    serverMessage = service.connectPlayer(session, com);
+                    break;
+                case LEAVE:
+                    serverMessage = service.leave(session, com);
+                    break;
+                case MAKE_MOVE:
+                    serverMessage = service.makeMove(session, (MakeMoveCommand) com);
+                    break;
+                case RESIGN:
+                    serverMessage = service.resign(session, com);
+                    break;
+                default:
+                    serverMessage = new ErrorMessage(ServerMessage.ServerMessageType.ERROR,
+                            "Error: Invalid request");
+            }
+            sendMessage(session, serverMessage);
         }
+        catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private void sendMessage(Session session, ServerMessage message) throws IOException {
+        String response = serializer.toJson(message);
+        session.getRemote().sendString(response);
     }
 
 }
--- server/src/main/java/service/WebSocketService.java
+++ server/src/main/java/service/WebSocketService.java
@@ -1,24 +1,46 @@
 package service;
 
+import chess.ChessMove;
 import dataaccess.AuthDAO;
 import dataaccess.GameDAO;
 import org.eclipse.jetty.websocket.api.Session;
+import websocket.commands.MakeMoveCommand;
 import websocket.commands.UserGameCommand;
 import websocket.messages.ServerMessage;
 
 public class WebSocketService {
     private final AuthDAO authDataAccess;
     private final GameDAO gameDataAccess;
+    int gameId;
+    String authToken;
     public WebSocketService(AuthDAO authDataAccess, GameDAO gameDataAccess){
         this.authDataAccess = authDataAccess;
         this. gameDataAccess = gameDataAccess;
     }
 
+    private void getInfo(UserGameCommand com){
+        gameId = com.getGameID();
+        authToken= com.getAuthToken();
+    }
+
     public ServerMessage connectPlayer(Session session, UserGameCommand com){
-        int gameId = com.getGameID();
-        String authToken = com.getAuthToken();
+        getInfo(com);
+        return null;
+    }
 
+    public ServerMessage leave(Session session, UserGameCommand com){
+        getInfo(com);
+        return null;
+    }
 
+    public ServerMessage makeMove(Session session, MakeMoveCommand com){
+        getInfo(com);
+        ChessMove move = com.getMove();
+        return null;
+    }
+
+    public ServerMessage resign(Session session, UserGameCommand com){
+        getInfo(com);
         return null;
     }
 
